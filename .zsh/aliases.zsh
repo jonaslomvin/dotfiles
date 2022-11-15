e8e0ad1ca7cae485a88a710d7816c50dcc1df7c7
Jonas Lomvin <16341370+jonaslomvin@users.noreply.github.com>
2022-11-15 15:59:46 +0100

    updated aliases with sort on directories

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,38 +1,25 @@
-
-# Alias
-# ---
-#
-# alias k="kubectl"
-# alias h="helm"
-# alias tf="terraform"
-# alias a="ansible"
-# alias ap="ansible-playbook"
-#alias dt="datree"
-
-# mac OS shortcuts
-alias code="open -a 'Visual Studio Code'"
-
-
-
 # ALIAS COMMANDS
+
 #alias ls="exa --icons --group-directories-first"
 #alias ll="exa --icons --group-directories-first -l"
-#alias g="goto"
-#alias grep='grep --color'
 
-#alias cbp="code /home/xcad/obsidianvault/boilerplates"
-#alias cpr="code /home/xcad/obsidianvault/projects"
-
-alias ls='exa -l' # ls
-alias lf='exa -lbF --git' # list, size, type, git
-alias ll='exa -lbGF --git' # long list
+alias ls='exa -l --group-directories-first' # ls
+alias lf='exa -lbF --git --group-directories-first' # list, size, type, git
+alias ll='exa -lbGF --git --group-directories-first' # long list
 alias llm='exa -lbGd --git --sort=modified' # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale' # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale --group-directories-firs' # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --group-directories-firs' # all + extended list
 
 # specialty views
-alias lS='exa -1' # one column, just names
+alias lS='exa -1 --group-directories-first' # one column, just names
 alias lt='exa --tree --level=2' # tree
 
 # change cat to use bat
 alias cat='bat'
+
+# mac OS shortcuts
+alias code="open -a 'Visual Studio Code'"
+
+# Obsidian
+#alias cbp="code /home/xcad/obsidianvault/boilerplates"
+#alias cpr="code /home/xcad/obsidianvault/projects"
